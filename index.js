27eda88f3e912f154f7aeca89112bd50cf1f9730
reagan <reaganm746@gmail.com>
2023-09-21 20:28:36 +0300

    assignment complet

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+  if (distance <= 400)
+    return 'This one is on me!';
+  
+  
+  else if (distance > 2000 && distance < 2500) 
+    return 'I will gladly take your thirty bucks.';
+  
+  else if (distance > 2500) {
+    return 'No can do.';
+  }
 }
 
 function ternaryCheckCity(){
   // Write your code here!
+  let message ="";
+  city === "NYC" ? message ="ok, sounds good." : message ="No go";
+
+  return message;
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  switch (tip) { case "generous" :return "thank you so much." ; break;
+
+  case "not as generous": return "Thank you." ;break ; default: return "Bye.";br
+}
 }
